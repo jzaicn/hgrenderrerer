04f64a63e273d721b8fc3bddff34edfcb9914acd
JackChen <jzaicn@163.com>
2017-03-21 20:00:46 +0800

    添加model获取器

---
+++ HGRenderEngine/RenderUI/getModel.bat
@@ -0,0 +1,16 @@
+mklink HG_BaseModel.cpp RenderDataExporter\HG_BaseModel.cpp
+mklink HG_BaseModel.h RenderDataExporter\HG_BaseModel.h
+mklink HG_Camera.cpp RenderDataExporter\HG_Camera.cpp
+mklink HG_Camera.h RenderDataExporter\HG_Camera.h
+mklink HG_Light.cpp RenderDataExporter\HG_Light.cpp
+mklink HG_Light.h RenderDataExporter\HG_Light.h
+mklink HG_Material.cpp RenderDataExporter\HG_Material.cpp
+mklink HG_Material.h RenderDataExporter\HG_Material.h
+mklink HG_Mesh.cpp RenderDataExporter\HG_Mesh.cpp
+mklink HG_Mesh.h RenderDataExporter\HG_Mesh.h
+mklink HG_MeshInstance.cpp RenderDataExporter\HG_MeshInstance.cpp
+mklink HG_MeshInstance.h RenderDataExporter\HG_MeshInstance.h
+mklink HG_ModelInstance.cpp RenderDataExporter\HG_ModelInstance.cpp
+mklink HG_ModelInstance.h RenderDataExporter\HG_ModelInstance.h
+mklink HG_SceneCenter.cpp RenderDataExporter\HG_SceneCenter.cpp
+mklink HG_SceneCenter.h RenderDataExporter\HG_SceneCenter.h
