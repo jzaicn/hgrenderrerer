6d90ac4fb1e35709e0027b5dd41d43ef39176da4
JackChen <jzaicn@163.com>
2017-03-22 10:22:51 +0800

    打开MFC漂亮模式

--- HGRenderEngine/RenderUI/stdafx.h
+++ HGRenderEngine/RenderUI/stdafx.h
@@ -54,6 +54,8 @@
 #else
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 
 
