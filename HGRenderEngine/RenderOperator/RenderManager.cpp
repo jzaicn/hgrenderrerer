10c3730b36008692f41cb239aabc02160aa49c97
JackChen <jzaicn@163.com>
2017-03-23 20:05:23 +0800

    基础导出接口

--- HGRenderEngine/RenderOperator/RenderManager.cpp
+++ HGRenderEngine/RenderOperator/RenderManager.cpp
@@ -19,6 +19,31 @@ public:
 private:
 	GETSET(EH_Context*,context);
 
+//////////////////////////////////////////////////////////////////////////
+// 字符管理工具
+public:
+	const char* createConstChar(const std::string& val)
+	{
+		const char* temptext = val.c_str();
+		char* new_char = (char*)malloc( sizeof(char) * (val.length() + 1) );
+		strcpy_s(new_char,(val.length() + 1),temptext);
+		m_charManager.push_back(new_char);
+		return new_char;
+	}
+	void clearCharBuffer()
+	{
+		for (int i = 0; i < m_charManager.size() ; i++)
+		{
+			free(m_charManager.at(i));
+			m_charManager[i] = NULL;
+		}
+		m_charManager.clear();
+	}
+private:
+	std::vector<char*> m_charManager;
+
+//////////////////////////////////////////////////////////////////////////
+// 填充器
 public:
 	void fill(uint_t& outval,UINT inval)
 	{
@@ -92,24 +117,24 @@ public:
 	{
 		outval.enabled = inval.get_sky_light_enable();
 		outval.intensity = inval.get_sky_light_intensity();
-		outval.hdri_name = inval.get_hdr().c_str();
+		outval.hdri_name = createConstChar(inval.get_hdr());
 		outval.hdri_rotation = inval.get_hdr_rotate();
 	}
 	void fill(EH_Texture& outval,std::string inval)
 	{
-		outval.filename = inval.c_str();
+		outval.filename = createConstChar(inval);
 		outval.repeat = 1;
 	}
 	void fill(EH_AssemblyInstance& outval,HG_ModelInstance inval)
 	{
-		outval.filename = inval.get_model_file().c_str();
+		outval.filename = createConstChar(inval.get_model_file());
 		fill(outval.mesh_to_world,inval.get_mesh_to_world());
 	}
 	void fill(EH_MeshInstance& outval,HG_MeshInstance inval)
 	{
-		outval.mesh_name = inval.get_material_code().c_str();
+		outval.mesh_name = createConstChar(inval.get_material_code());
 		fill(outval.mesh_to_world,inval.get_mesh_to_world());
-		outval.mtl_name = inval.get_material_code().c_str();
+		outval.mtl_name = createConstChar(inval.get_material_code());
 	}
 	void fill(EH_Mesh& outval,HG_Mesh inval)
 	{
@@ -119,61 +144,68 @@ public:
 		for (int i = 0; i < inval.get_verts().size() ; i++)
 		{
 			fill((EH_Vec)(outval.verts[i]),inval.get_verts().at(i));
-		}
-		
-		int indexFace = 0;
-		for (int i = 0; i < inval.get_faces().size() ; i++)
-		{
 			fill((EH_Vec)(outval.normals[i]),inval.get_normals().at(i));
 			fill((EH_Vec2)(outval.uvs[i]),inval.get_uvs().at(i));
-			fill((uint_t)(outval.face_indices[indexFace++]),inval.get_faces().at(i).get_t1());
-			fill((uint_t)(outval.face_indices[indexFace++]),inval.get_faces().at(i).get_t2());
-			fill((uint_t)(outval.face_indices[indexFace++]),inval.get_faces().at(i).get_t3());
+		}
+		
+		
+		for (int i = 0; i < inval.get_faces().size() ; i++)
+		{
+			fill((uint_t)(outval.face_indices[i+0]),inval.get_faces().at(i).get_t1());
+			fill((uint_t)(outval.face_indices[i+1]),inval.get_faces().at(i).get_t2());
+			fill((uint_t)(outval.face_indices[i+2]),inval.get_faces().at(i).get_t3());
 		}
 	}
 
 	void fill(EH_Material& outval,HG_Material inval)
 	{
 		outval.backface_cull = inval.get_cull_back();
-		outval.diffuse_weight = 1.0;
+		
 
-		if (inval.get_type() == HG_Material::color)
-		{
-			fill((EH_RGB)outval.diffuse_color,inval.get_color());
-		}
-		if (inval.get_type() == HG_Material::image)
-		{
-			fill(outval.diffuse_tex,inval.get_image());
-		}
-// 		
-// 		outval.roughness;
-// 		outval.backlight;
-// 
-// 		outval.specular_weight;
-// 		outval.specular_color ;
-// 		outval.specular_tex;
-// 		outval.glossiness;
-// 		outval.specular_fresnel;
-// 		outval.anisotropy;
-// 		outval.rotation;
-// 
-// 		outval.transp_weight;
-// 		outval.transp_invert_weight;
-// 		outval.transp_tex;
-// 
-// 		outval.bump_weight;
-// 		outval.bump_tex;
-// 		outval.normal_bump;
-// 
-// 		outval.mirror_weight;
-// 		outval.mirror_color;
-// 		outval.mirror_fresnel;
-// 
-// 		outval.refract_weight;
-// 		outval.refract_invert_weight;
-// 		outval.refract_color;
-// 		outval.ior;
-// 		outval.refract_glossiness;
+		fill(outval.diffuse_color,HG_Vec4());
+		outval.diffuse_tex.filename = NULL;
+		outval.diffuse_tex.repeat = 1.0;
+// 		if (inval.get_type() == HG_Material::color)
+// 		{
+// 			fill((EH_RGB)outval.diffuse_color,inval.get_color());
+// 		}
+// 		if (inval.get_type() == HG_Material::image)
+// 		{
+// 			fill(outval.diffuse_tex,inval.get_image());
+// 		}
+		
+		outval.diffuse_weight = 1.0;
+		outval.roughness = 1.0;
+		outval.backlight = 1.0;
+
+		outval.specular_weight = 1.0;
+		fill(outval.specular_color,HG_Vec4());
+		outval.specular_tex.filename = NULL;
+		outval.specular_tex.repeat = 1.0;
+		outval.glossiness = 1.0;
+		outval.specular_fresnel = 1.0;
+		outval.anisotropy = 1.0;
+		outval.rotation = 1.0;
+
+		outval.transp_weight = 1.0;
+		outval.transp_invert_weight = 1.0;
+		outval.transp_tex.filename = NULL;
+		outval.transp_tex.repeat = 1.0;
+
+		outval.bump_weight = 1.0;
+		outval.bump_tex.filename = NULL;
+		outval.bump_tex.repeat = 1.0;
+		outval.normal_bump = 1.0;
+
+		outval.mirror_weight = 1.0;
+		fill(outval.mirror_color,HG_Vec4());
+		outval.mirror_fresnel = 1.0;
+
+		outval.refract_weight = 1.0;
+		outval.refract_invert_weight = 1.0;
+		fill(outval.refract_color,HG_Vec4());
+		outval.ior = 1.0;
+		outval.refract_glossiness = 1.0;
 	}
 
 
@@ -209,6 +241,12 @@ void RenderManager::SaveESS(std::string path)
 	
 	storage.initWhenNot();
 
+	EH_ExportOptions opt;
+	opt.base85_encoding = true;
+	opt.left_handed = false;
+
+	EH_begin_export(storage.get_context(),get_scene_path().c_str(),&opt);
+
 	// mesh 列表
 	for (int i = 0; i < HG_SceneCenter::inst().ref_meshList().size() ; i++)
 	{
@@ -229,13 +267,13 @@ void RenderManager::SaveESS(std::string path)
 	}
 
 	// 材质 列表
-	for (int i = 0; i < HG_SceneCenter::inst().ref_materialList().size() ; i++)
-	{
-		HG_Material hg_material = HG_SceneCenter::inst().ref_materialList().at(i);
-		EH_Material material;
-		storage.fill(material,hg_material);
-		EH_add_material(storage.get_context(),hg_material.get_unique_code().c_str(),&material);
-	}
+// 	for (int i = 0; i < HG_SceneCenter::inst().ref_materialList().size() ; i++)
+// 	{
+// 		HG_Material hg_material = HG_SceneCenter::inst().ref_materialList().at(i);
+// 		EH_Material material;
+// 		storage.fill(material,hg_material);
+// 		EH_add_material(storage.get_context(),hg_material.get_unique_code().c_str(),&material);
+// 	}
 
 	// mesh 实例 列表
 	for (int i = 0; i < HG_SceneCenter::inst().ref_meshInstanceList().size() ; i++)
@@ -266,6 +304,10 @@ void RenderManager::SaveESS(std::string path)
 	storage.fill(sky,HG_SceneCenter::inst().get_sky());
 	EH_set_sky(storage.get_context(),&sky);
 
+
+	EH_end_export(storage.get_context());
+
+	storage.clearCharBuffer();
 }
 
 void RenderManager::Begin()
