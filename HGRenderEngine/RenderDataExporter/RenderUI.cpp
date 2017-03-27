98c6f050bfc0cc4bffa1e2bcb43bdebf08dfec79
JackChen <jzaicn@163.com>
2017-03-27 16:33:06 +0800

    执行程序

--- HGRenderEngine/RenderDataExporter/RenderUI.cpp
+++ HGRenderEngine/RenderDataExporter/RenderUI.cpp
@@ -30,22 +30,44 @@ void RenderUI::showRenderDlg()
 	//获得整个场景的根
 	osg::Group* root = sm->get3DScene();
 	
+	//设置读取整个场景
+#if 0
 	//遍历整个场景
 	HGSceneNodeVisitor vistor;
 	//root->accept(vistor);
 	HGLOG_DEBUG("scene ok");
+#endif // 0
 
+	//设置读取外部ive模型
+#if 1	
 	//osg::Node *node00 = osgDB::readNodeFile( "E:\\XIANGMUSOFT\\TYBSOFT2016-back\\Ironware\\连接类\\层板托(衣柜)\\板拖.IVE" );
 	osg::Node *node00 = osgDB::readNodeFile( "D:\\123123123\\trian3.ive" );
 	if (node00)
 	{
-		//node00->accept(vistor);
+		HGSceneNodeVisitor vistor;
+		node00->accept(vistor);
 		HGLOG_DEBUG("load ok");
 	}
+#endif // 1
 	
-	//定义输出json格式
-	Json::Value output;
+	
 
+	//获得摄像机坐标
+#if 0
+	osg::Camera* cam = sm->getHUD();
+	osg::Matrix camMatrix = cam->getViewMatrix();
+	HG_Mat worldMatrix = HG_Mat(
+		HG_Vec4(camMatrix(0,0),camMatrix(0,1),camMatrix(0,2),camMatrix(0,3)),
+		HG_Vec4(camMatrix(1,0),camMatrix(1,1),camMatrix(1,2),camMatrix(1,3)),
+		HG_Vec4(camMatrix(2,0),camMatrix(2,1),camMatrix(2,2),camMatrix(2,3)),
+		HG_Vec4(camMatrix(3,0),camMatrix(3,1),camMatrix(3,2),camMatrix(3,3))
+	);
+	HG_Camera camera;
+	camera.set_view_to_world(worldMatrix);
+#endif
+
+	//设置读取外部模型，并设置摄像机
+#if 0
 	HG_SceneCenter::inst().addModelInstance(HG_ModelInstance("D:\\picture.ess",HG_Mat()));
 	HG_Camera camera;
 	camera.set_view_to_world(
@@ -60,12 +82,20 @@ void RenderUI::showRenderDlg()
 	camera.set_far_clip(1000.0);
 	camera.set_near_clip(0.0);
 	HG_SceneCenter::inst().addCamera(camera);
+#endif
+
+
+
+	//定义输出json格式
+	Json::Value output;
 	HG_SceneCenter::inst().save(output);
 	
 
-
+	//保存到文件
+#if 1
 	Json::FastWriter fastwriter;
-	std::string result = fastwriter.write(output);
+	//std::string result = fastwriter.write(output);
+	std::string result = output.toStyledString();
 	std::ofstream  writestream("d:\\room.json");
 	if (writestream.is_open())
 	{
@@ -73,6 +103,10 @@ void RenderUI::showRenderDlg()
 		writestream.flush();
 		writestream.close();
 	}
+#else
+	//打印成字符串
+	HGLOG_DEBUG(output.toStyledString());
+#endif
 	
 }
 
