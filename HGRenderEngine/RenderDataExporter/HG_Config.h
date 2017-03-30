4a4028d56ed6fe3afe18bb70e6c561cd53136d5e
JackChen <jzaicn@163.com>
2017-03-30 15:48:39 +0800

    添加配置项和math工具类

---
+++ HGRenderEngine/RenderDataExporter/HG_Config.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "HG_BaseModel.h"
+
+class HG_Config
+{
+private:
+	HG_Config(void)
+	{
+		//需要设置到\\结束 ex: D:\\***\\
+		m_export_path = ("D:\\export\\");
+		m_texture_relative_path = ("textures\\");
+	}
+
+	~HG_Config(void)
+	{
+
+	}
+public:
+
+	HG_Config& inst()
+	{
+		return m_config;
+	}
+	
+	std::string get_texture_export_path(std::string texture_name)
+	{
+		return m_export_path + m_texture_relative_path + texture_name;
+	}
+
+private:
+	static HG_Config m_config;
+	std::string m_export_path;
+	std::string m_texture_relative_path;
+};
+
---
+++ HGRenderEngine/RenderDataExporter/HG_Math.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "HG_Math.h"
+
+
+HG_Math::HG_Math(void)
+{
+}
+
+
+HG_Math::~HG_Math(void)
+{
+}
